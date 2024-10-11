0239aaad7f123847f5a25f1b0cb590c6fc533f83
Luis Correia <luis.correia@jscrambler.com>
2024-10-11 11:42:46 +0100

    add initial code

---
+++ m2.py
@@ -0,0 +1,46 @@
+# Importing Libraries 
+import serial 
+import time 
+import matplotlib.pyplot as plt
+# from pySerialTransfer import pySerialTransfer as txfer
+
+arduino = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, timeout=0.1)
+time.sleep(5)
+
+def write_read(x): 
+	arduino.write(bytes(x, 'utf-8'))
+	initTime = time.time()
+	# arduino.flush() 
+	# time.sleep(0.1) 
+	while arduino.in_waiting == 0:
+		pass
+
+	endTime = time.time()
+	delta = endTime - initTime
+
+	data = arduino.readline()
+	
+	return (data, delta)
+
+passwd = "a"
+strlen = []
+while len(passwd) < 21:
+	print(arduino.readline())
+	resp = write_read(passwd + "\n")
+	print(resp[0])
+	strlen.append({"length": len(passwd), "time": resp[1]})
+	passwd += "a"
+	
+print(str(strlen) + "\n\n\n\n\n\n\n\n")
+
+numList = []
+timesList = []
+for item in strlen:
+	print(f"Number of chars: { item['length'] }\nTime: {item['time']}\n\n")
+	numList.append(item['length'])
+	timesList.append(item['time'])
+
+plt.plot(numList,timesList)
+plt.xlabel('Number of chars')
+plt.ylabel('Time')
+plt.show()
